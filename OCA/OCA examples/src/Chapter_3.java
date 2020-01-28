ebd8fb6bf9a0898e990e902b600428c25172db10
Kuanysh <mrkunyabai@gmail.com>
2020-01-29 00:10:27 +0600

    First example

--- OCA/OCA examples/src/Chapter_3.java
+++ OCA/OCA examples/src/Chapter_3.java
@@ -1,7 +1,13 @@
 
+import java.util.*;
+
 public class Chapter_3 {
+	/**
+	 * @param args
+	 */
 	public static void main(String args[]) {
-		int[][] twoD = new int[3][2];
+		//multidimensional array
+		/*int[][] twoD = new int[3][2];
 		for(int i = 0; i < twoD.length; i++) {
 			for(int j = 0; j <  twoD[i].length;j++) {
 				System.out.println(twoD[i][j] + " ");
@@ -14,7 +20,29 @@ public class Chapter_3 {
 				System.out.println(num + " ");
 			}
 			System.out.println();
-		}
+		}*/
+		//Autoboxing
+		
+		List<Double> weights = new ArrayList<>();
+		weights.add(50.5);
+		weights.add(new Double(60));
+		weights.remove(50.5);
+		double first = weights.get(0);
+		System.out.println(first);
+		
+		List<Integer> numbers = new ArrayList<>();
+		numbers.add(1);
+		numbers.add(2);
+		numbers.remove(1);
+		System.out.println(numbers);
+		
+		List<String> list = new ArrayList<>();
+		list.add("hawk");
+		list.add("robin");
+		Object[] objectArray = list.toArray();
+		System.out.println(objectArray.length);
+		String[] stringArray = list.toArray(new String[0]);
+		System.out.println(stringArray.length);
 		
 	}
 }
