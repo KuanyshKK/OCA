c47274a330b15b9d3da601777ab88b9cdda18ee8
Kuanysh <mrkunyabai@gmail.com>
2020-01-29 18:38:45 +0600

    Continue

--- OCA/OCA examples/src/Chapter_3.java
+++ OCA/OCA examples/src/Chapter_3.java
@@ -23,7 +23,7 @@ public class Chapter_3 {
 		}*/
 		//Autoboxing
 		
-		List<Double> weights = new ArrayList<>();
+		/*List<Double> weights = new ArrayList<>();
 		weights.add(50.5);
 		weights.add(new Double(60));
 		weights.remove(50.5);
@@ -43,6 +43,21 @@ public class Chapter_3 {
 		System.out.println(objectArray.length);
 		String[] stringArray = list.toArray(new String[0]);
 		System.out.println(stringArray.length);
+		*/
 		
+		String[] array = {"hawk", "robin"};
+		List<String> list = Arrays.asList(array);
+		System.out.println(list.size());
+		list.set(1, "test");
+		array[0] = "new";
+		for(String b : array) System.out.println(b + " ");
+		//list.remove(1); we can't change the size of the list
+		
+		List<Integer> numbers = new ArrayList<>();
+		numbers.add(99);
+		numbers.add(5);
+		numbers.add(81);
+		Collections.sort(numbers);
+		System.out.println(numbers);
 	}
 }
